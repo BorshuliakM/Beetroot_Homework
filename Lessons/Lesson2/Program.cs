1e07a6cfec81cfb8cbdc29eb1ded175f015889f6
BorshuliakM <borshuliakm@gmail.com>
2023-03-05 06:17:26 +0200

    code added

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿try
+{
+    int result = 0;
+    Console.Write("Input x = ");
+    int x = int.Parse(Console.ReadLine());
+    Console.Write("Input y = ");
+    int y = int.Parse(Console.ReadLine());
+    if (x > y)
+    {
+        for (int i = y; i <= x; i++)
+        {
+            result += i;
+        }
+    }
+    else if (x < y)
+    {
+        for (int i = x; i <= y; i++)
+        {
+            result += i;
+        }
+    }
+    else result = x;
+    Console.WriteLine("Result " + result);
+    Console.ReadKey();
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid Input");
+}
