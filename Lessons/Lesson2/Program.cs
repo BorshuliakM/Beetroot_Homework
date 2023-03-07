1a04c4dc8c5320cc51a2e6e58e129bd674ed7883
BorshuliakM <borshuliakm@gmail.com>
2023-03-07 14:46:27 +0200

    2 tasks done

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿Console.WriteLine(Max(7));
+Console.WriteLine(Max(7,8,9));
+Console.WriteLine(Min(7, 8));
+
+int Max(params int[] numbers) => numbers.Max();
+
+int Min(params int[] numbers) => numbers.Min();
