68e1b00762487f1e67d2b73db18b5aca428b07ae
Mykola Borshuliak <borshuliakm@gmail.com>
2023-03-08 08:59:34 +0200

    some changes

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,7 +1,50 @@
-﻿Console.WriteLine(Max(7));
-Console.WriteLine(Max(7,8,9));
-Console.WriteLine(Min(7, 8));
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum;
+        Console.WriteLine(Max(7, 8));
+        Console.WriteLine(Max(6, 4, 8));
+        Console.WriteLine(Min(4, 8));
+        Console.WriteLine(Min(7, 3, 8));
+        var x = TrySumIfOdd(5, 9, out sum);
+        Console.WriteLine($"{x}\n{sum}");
+        Console.WriteLine(Repeat("str", 4));
+    }
+    static int Max(int a, int b)
+    {
+        return Math.Max(a, b);
+    }
+    static int Max(int a, int b, int c)
+    {
+        return Math.Max(a, Math.Max(b, c));
+    }
+    static int Min(int a, int b)
+    {
+        return Math.Min(a, b);
+    }
+    static int Min(int a, int b, int c)
+    {
+        return Math.Min(a, Math.Min(b, c));
+    }
 
-int Max(params int[] numbers) => numbers.Max();
-
-int Min(params int[] numbers) => numbers.Min();
+    static bool TrySumIfOdd(int a, int b, out int sum)
+    {
+        sum = 0;
+        for (int i = a; i <= b; i++)
+        {
+            sum += i;
+        }
+        if (sum % 2 != 0) return true;
+        else return false;
+    }
+    static string Repeat(string X, int N)
+    {
+        string str = "";
+        for (int i = 0; i < N; i++)
+        {
+            str += X;
+        }
+        return str;
+    }
+}
