86098e232061addc4b59a78616ad4a28e6d06143
BorshuliakM <borshuliakm@gmail.com>
2023-03-12 20:13:53 +0200

    some code added

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = new int[10];
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{i + 1} = ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+foreach(int i in numbers)
+{ 
+    Console.WriteLine($"[{i}]"); 
+}
 Console.ReadKey();
