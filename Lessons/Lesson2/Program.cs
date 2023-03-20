00b86f1e704f12d75e7d68c65aae415404a58005
Mykola Borshulyak <borshuliakm@gmail.com>
2023-03-20 17:38:35 +0200

    Test

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+        var str = "Hello, World";
+
+        Console.WriteLine(str.IndexOf("hello"));
+    }
+}
+public class Person
+{
+    private string Name;
+    private string LastName;
+    private string Phonenumber;
+    public void Input()
+    {
+
+    }
+}
