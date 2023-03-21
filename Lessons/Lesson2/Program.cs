2f20321e06119a849e796d22152f386f7968d9a6
Mykola Borshulyak <borshuliakm@gmail.com>
2023-03-21 03:05:29 +0200

    Input and search nethod added

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,21 +1,39 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadKey();
-        var str = "Hello, World";
-
-        Console.WriteLine(str.IndexOf("hello"));
+        FileInput("Євген Ш.Л.", "+380977489712");
+        Search("Євген");
     }
-}
-public class Person
-{
-    private string Name;
-    private string LastName;
-    private string Phonenumber;
-    public void Input()
-    {
 
+    private static void FileInput(string Fullname, string PhoneNumber)
+    {
+        string name_pattern = @"^[А-ЩЬЮЯҐЄІЇ][а-щьюяґєії']+\s[А-ЩЬЮЯҐЄІЇ]\.[А-ЩЬЮЯҐЄІЇ]\.$";
+        string phone_pattern = @"\+380\d{9}";
+        Regex name = new Regex(name_pattern);
+        Regex phone = new Regex(phone_pattern);
+        if (name.IsMatch(Fullname) && phone.IsMatch(PhoneNumber)) File.AppendAllText("C:\\Users\\borsh\\Source\\Repos\\BorshuliakM\\Beetroot_Homework\\Lessons\\Lesson2\\TelephoneBook.txt", 
+            Fullname+" "+PhoneNumber);
+        else Console.WriteLine("Incorrect input");
+    }
+    private static void Search(string input)
+    {
+        string[] users = File.ReadAllLines("C:\\Users\\borsh\\Source\\Repos\\BorshuliakM\\Beetroot_Homework\\Lessons\\Lesson2\\TelephoneBook.txt");
+        List<string> matches = new List<string>();
+        foreach (var user in users)
+        {
+            if (user.Contains(input))
+            {
+                matches.Add(user);
+            }
+        }
+
+        Console.WriteLine("Found users: ");
+        foreach (var user in matches)
+        {
+            Console.WriteLine(user);
+        }
     }
 }
