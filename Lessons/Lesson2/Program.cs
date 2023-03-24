b1bf8fe7589618bab0bdc32715d06a770e382cb8
Mykola Borshulyak <borshuliakm@gmail.com>
2023-03-24 05:18:01 +0200

    Class Author, Book added

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using Lesson;
+
+Book Harry_Potter = new(0, "Harry Potter",  );
+Book_library Somelibrary = new Book_library("library_1",  );
