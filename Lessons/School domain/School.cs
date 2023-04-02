f2db9aca6d9b919ddb0ba39901c8250b4640b9ad
Mykola Borshulyak <borshuliakm@gmail.com>
2023-04-02 11:07:26 +0300

    ,

--- Lessons/School domain/School.cs
+++ Lessons/School domain/School.cs
@@ -2,7 +2,20 @@
 {
     public class School
     {
-        required int school_number; //думаю номер школи не змінний тому init
-        public School() { }
+        private string school_name;
+        private string school_type;
+
+
+        public School(string school_name, string school_type) 
+        { 
+            this.school_name = school_name;
+            this.school_type = school_type;
+        }
+        public void ChangeName(string name, string type)
+        {
+            name=school_name;
+            type=school_type;
+        }
+
     }
 }
