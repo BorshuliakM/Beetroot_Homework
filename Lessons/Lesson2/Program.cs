e39aea5e7cdb3ca5557fda3819af9e89162202e6
BorshuliakM <borshuliakm@gmail.com>
2023-04-07 03:39:21 +0300

    Test upcast

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -2,25 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        A b = new B();
-
-        Console.WriteLine(b.Method());
+     
     }
 }
-public class A
-
-{
-
-    public string Method() => "Call A";
-
-}
 
 
 
-public class B : A
 
-{
-
-    public string Method() => "Call B";
-
-}
