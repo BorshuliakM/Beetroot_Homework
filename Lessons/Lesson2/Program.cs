01af9a2655f3373249067de2b3fd590596ab284f
BorshuliakM <borshuliakm@gmail.com>
2023-04-09 17:16:08 +0300

    done

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        int[] arr = new int[4];
+        stack.Push(47);
+        stack.Push(4);
+        stack.Push(8);
+        Console.WriteLine("Count = " + stack.Count());
+        Console.WriteLine("Peek = " + stack.Peek());
+        stack.CopyTo(arr, 0);
+        foreach (var k in arr)
+        {
+            Console.WriteLine(k);
+        }
+    }
+}
---
+++ Lessons/Lesson2/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    public class Stack<T>
+    {
+        private List<T> _items;
+        public Stack() => this._items = new List<T>();
+        public void Push(T item) => this._items.Add(item);
+
+        public T Pop()
+        {
+            if (this._items.Count == 0) throw new InvalidOperationException("Stack is empty.");
+            T item = this._items[^1];
+            this._items.RemoveAt(this._items.Count - 1);
+            return item;
+        }
+        public void Clear()
+        {
+            _items.Clear();
+        }
+        public int Count()
+        {
+            if (_items.Count==0) Console.WriteLine("Stack is empty.");
+            return _items.Count;
+        }
+        public T Peek()
+        {
+            if (this._items.Count == 0) throw new InvalidOperationException("Stack is empty.");
+            return this._items[^1];
+        }
+        public T[] CopyToArr(T[] array)
+        {
+            if (this._items.Count == 0) throw new InvalidOperationException("Stack is empty.");
+            _items.CopyTo(array);
+            return array;
+        }
+    }
+}
