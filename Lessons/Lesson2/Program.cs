718829ece4661e3a6bfa88b4af72501ca21ebd50
Mykola Borshulyak <borshuliakm@gmail.com>
2023-05-05 06:30:34 +0300

    2 tasks done

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using Newtonsoft.Json;
+
+namespace Lesson_20;
+
+internal class Program
+{
+    /*
+    find out who is located farthest north/south/west/east using latitude/longitude data
+    find max and min distance between 2 persons
+    find 2 persons whos ‘about’ have the most same words
+    find persons with same friends(compare by friend’s name)
+    */
+
+    private static void Main(string[] args)
+    {
+        var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+    }
+}
