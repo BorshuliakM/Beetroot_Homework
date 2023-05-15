49d3d0ebf5812667e8547710660ff498fed1a928
Mykola Borshulyak <borshuliakm@gmail.com>
2023-05-15 03:30:16 +0300

    .

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace Lesson_21;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        //Create application that will load assembly
+        //and show all it’s classes and their methods with arguments needed to pass
+        //and return types
+
+        Assembly asm = Assembly.LoadFrom("Lesson_21.dll");
+        Console.WriteLine(asm.FullName);
+        Type[] types = asm.GetTypes();
+        foreach (Type k in types)
+        {
+            Console.WriteLine(k.Name);
+        }
+
+
+       /*TestClass t = new();
+       Type myType = typeof(TestClass);
+       /*foreach (MemberInfo member in myType.GetMembers())
+       {
+           Console.WriteLine($"{member.DeclaringType} {member.MemberType} {member.Name}");
+       }
+       t.Print();
+       var testProp = myType.GetProperty("TestProperty2");
+       var prop = testProp?.GetValue(t);
+       testProp?.SetValue(t, 22);
+       t.Print();*/
+    }
+}
---
+++ Lessons/Lesson2/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_21;
+
+internal class TestClass
+{
+    public int TestProperty { get; set; }
+    public int TestProperty2 { get; set; }
+    public TestClass()
+    {
+        TestProperty = 1;
+        TestProperty2 = 2;
+    }
+    public int TestMethodAddition(int testArg, int testArg2) => testArg + testArg2;
+    public void Print() => Console.WriteLine($"TestProperty = {TestProperty}\nTestProperty2 = {TestProperty2}");
+}
