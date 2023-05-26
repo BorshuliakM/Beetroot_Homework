5328d061e82ec021e9ee06107c9690ffe5dfe684
Mykola Borshulyak <borshuliakm@gmail.com>
2023-05-26 19:05:00 +0300

    model done

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using Lesson24;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://inshorts.deta.dev/news?category=science");
+        string content = await response.Content.ReadAsStringAsync();
+        NewsModel news = await response.Content.ReadFromJsonAsync<NewsModel>();
+    }
+}
