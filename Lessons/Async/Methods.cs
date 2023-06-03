43652ecf3611b4f89a1e803cce802b6d344c5e1f
Mykola Borshulyak <borshuliakm@gmail.com>
2023-06-03 19:59:12 +0300

    .

---
+++ Lessons/Async/Methods.cs
@@ -0,0 +1,42 @@
+﻿namespace MethodsHomework;
+
+public static class Methods
+{
+    public static int Max(int a, int b)
+    {
+        return Math.Max(a, b);
+    }
+    public static int Max(int a, int b, int c)
+    {
+        return Math.Max(a, Math.Max(b, c));
+    }
+    public static int Min(int a, int b)
+    {
+        return Math.Min(a, b);
+    }
+    public static int Min(int a, int b, int c)
+    {
+        return Math.Min(a, Math.Min(b, c));
+    }
+
+    public static bool TrySumIfOdd(int a, int b, out int sum)
+    {
+        sum = 0;
+        for (int i = a; i <= b; i++)
+        {
+            sum += i;
+        }
+        if (sum % 2 != 0) return true;
+        else return false;
+    }
+    public static string Repeat(string X, int N)
+    {
+        string str = "";
+        for (int i = 0; i < N; i++)
+        {
+            str += X;
+        }
+        return str;
+    }
+}
+
--- Lessons/Lesson2/Tests.cs
+++ Lessons/Lesson2/Tests.cs
@@ -1,29 +1,84 @@
 ﻿using Xunit;
-using Async;
-using System.Text.Json;
+using MethodsHomework;
 
-namespace TestAsync;
+namespace MethodsTest;
 public class Tests
 {
     [Theory]
-    [InlineData("all")]
-    private static async Task<NewsModel?> GetData(string categoryName)
+    [InlineData(5, 10, 10)]
+    [InlineData(15, 8, 15)]
+    [InlineData(-5, -10, -5)]
+    public void Max_TwoIntegers_ReturnsMaximum(int a, int b, int expected)
     {
-        try
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync($"https://inshorts.deta.dev/news?category={categoryName}");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                NewsModel newsByCategory = JsonSerializer.Deserialize<NewsModel>(responseBody);
-                return newsByCategory;
-            }
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine(e.Message);
-            return null;
-        }
+        // Act
+        int result = Methods.Max(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 10, 15, 15)]
+    [InlineData(15, 8, 20, 20)]
+    [InlineData(-5, -10, -15, -5)]
+    public void Max_ThreeIntegers_ReturnsMaximum(int a, int b, int c, int expected)
+    {
+        // Act
+        int result = Methods.Max(a, b, c);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 10, 5)]
+    [InlineData(15, 8, 8)]
+    [InlineData(-5, -10, -10)]
+    public void Min_TwoIntegers_ReturnsMinimum(int a, int b, int expected)
+    {
+        // Act
+        int result = Methods.Min(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 10, 15, 5)]
+    [InlineData(15, 8, 20, 8)]
+    [InlineData(-5, -10, -15, -15)]
+    public void Min_ThreeIntegers_ReturnsMinimum(int a, int b, int c, int expected)
+    {
+        // Act
+        int result = Methods.Min(a, b, c);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 10, true)]
+    [InlineData(2, 10, false)]
+    [InlineData(5, 5, true)]
+    public void TrySumIfOdd_ReturnsTrueIfSumIsOdd(int a, int b, bool expected)
+    {
+        // Act
+        bool result = Methods.TrySumIfOdd(a, b, out _);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("Hello", 3, "HelloHelloHello")]
+    [InlineData("ABC", 0, "")]
+    [InlineData("", 5, "")]
+    public void Repeat_ReturnsRepeatedString(string X, int N, string expected)
+    {
+        // Act
+        string result = Methods.Repeat(X, N);
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
